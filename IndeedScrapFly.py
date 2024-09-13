7ce93d17b38bbc5011f67192bd5f4dd15300ceac
Casian Sebastian Chiriac <S.C.Chiriac@student.tudelft.nl>
2024-09-13 18:27:00 +0200

    parsed output from ScrapFly

--- IndeedScrapFly.py
+++ IndeedScrapFly.py
@@ -1,6 +1,8 @@
 import re
 import json
 import sys
+from datetime import timezone, datetime
+
 import mysql.connector
 from scrapfly import ScrapflyClient, ScrapeConfig
 from bs4 import BeautifulSoup
@@ -11,31 +13,56 @@ lang = "java"
 url="https://nl.indeed.com/jobs?q="+lang+"&l=netherlands&start=0"
 i = 0
 while(jobs := get_page_data(url)["results"]):
-    break
     for job in jobs:
         #print(job.keys())
-        print(job["createDate"])
-        print(job["jobLocationCity"])
-        print(job["title"])
-        print(job["salarySnippet"])
-        print(job["jobCardRequirementsModel"])
-        print('----------------------------------------\n')
+        #print(job["jobCardRequirementsModel"])
+        try:
+            id = (job["adId"])
+            timestamp_ms = job["createDate"]
+            dt = datetime.fromtimestamp(timestamp_ms/1000.0, tz=timezone.utc)
+            mysqlDate = dt.strftime('%Y-%m-%d %H:%M:%S')
+            city = job["jobLocationCity"]
+        except:
+            id = None
+            mysqlDate = None
+            city = None
+
+        try:
+            title = job["title"]
+        except:
+            title = None
+        salaryNum = None
+        try:
+            salaryString = job["salarySnippet"]["text"]
+            print(salaryString)
+            salary = re.findall(r'\d+', salaryString)
+            numbers = []
+            if(len(salary) == 4):
+                numbers = [int(salary[0] + salary[1]), int(salary[2] + salary[3])]
+            elif(len(salary) == 2):
+                numbers = [int(salary[0]), int(salary[1])]
+            else:
+                numbers = [int(salary[0])]
+            if(numbers is not None):
+                if(len(numbers) == 2 and numbers[1] <= 20000): # if the salary is < 20k then its monthly
+                    salaryNum = int((numbers[0] + numbers[1]) / 2)
+                elif(len(numbers) == 2 and numbers[1] > 20000):
+                    salaryNum = int((numbers[0] + numbers[1]) / 24) # if the salary is > 20k then its yearly
+                else:
+                    salaryNum = numbers[0] # if there is only one number then its the salary
+        except:
+            print("No salary given")
+            salaryNum = None
     url = next_page(url)
     i += 1
-    if(i > 0):
+    if(i > 1):
         break
 
+sys.exit()
+
 cursor, indeed_db = connect_to_mysql("localhost", 3406, "root", "12345rita", "indeed_db")
-if(indeed_db == None):
-    print("Error connecting to mysql")
-# Insert data
-query = "INSERT INTO jobstable (id, lang, assoc_lang, salary, date, lvl) VALUES (%s, %s, %s, %s, %s, %s)"
 data = (12, 1, 5, 3200, "2021-09-12", 3)
-cursor.execute(query, data)
-
-# Commit the transaction
-indeed_db.commit()
-
+insert_into_jobstable(cursor, indeed_db, data)
 cursor.close()
 indeed_db.close()
 
